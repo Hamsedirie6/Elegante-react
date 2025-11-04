72cac645e5e06b05c86073374dce178f0a9f9d3e
hamsedirie6 <hamse_03@hotmail.se>
2025-11-04 15:22:05 +0100

    ...

--- src/views/Checkout.tsx
+++ src/views/Checkout.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import { api } from '../services/api';
 
+
 export default function Checkout() {
   const { items, total, clear } = useCart();
   const navigate = useNavigate();
